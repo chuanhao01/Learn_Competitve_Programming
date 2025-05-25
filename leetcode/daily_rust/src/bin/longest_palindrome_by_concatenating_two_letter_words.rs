5e3e26f31469c4a9a0a10e5f2c9e9ae1a0ddf900
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2025-05-25 23:48:06 +0800

    [feat] 250525 done

--- leetcode/daily_rust/src/bin/longest_palindrome_by_concatenating_two_letter_words.rs
+++ leetcode/daily_rust/src/bin/longest_palindrome_by_concatenating_two_letter_words.rs
@@ -1,19 +1,30 @@
 //https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words/?envType=daily-question&envId=2025-05-02
 struct Solution {}
-use std::collections::HashSet;
+use std::collections::HashMap;
 impl Solution {
     pub fn longest_palindrome(words: Vec<String>) -> i32 {
         let mut ans = 0;
-        let mut opposite: HashSet<String> = HashSet::new();
+        let mut opposite: HashMap<String, i64> = HashMap::new();
         for word in words {
-            if opposite.contains(&word) {
-                opposite.remove(&word);
-                ans += 4;
-            } else {
-                opposite.insert(word.chars().rev().collect::<String>());
+            // Check if we can remove it as a pair
+            match opposite.get(&word) {
+                Some(wc) => {
+                    if *wc == 1 {
+                        opposite.remove(&word);
+                    } else {
+                        opposite.entry(word).and_modify(|wc| *wc -= 1);
+                    }
+                    ans += 4;
+                }
+                None => {
+                    opposite
+                        .entry(word.chars().rev().collect::<String>())
+                        .and_modify(|wc| *wc += 1)
+                        .or_insert(1);
+                }
             }
         }
-        for word in opposite {
+        for (word, _) in opposite {
             let mut w = word.chars();
             if w.next().unwrap() == w.next().unwrap() {
                 ans += 2;
